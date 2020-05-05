0b9b4dfa44a94e08a894d337c2de352b60054084
Sol Zilberman <solzilbermancode@gmail.com>
2020-05-04 21:44:05 -0500

    Updates

---
+++ precache-manifest.0064518a9490bb9844b20ca7d8582023.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96257924924dd9f128f51c2f41a7f59e",
+    "url": "/portfolioSite/index.html"
+  },
+  {
+    "revision": "9aff79dcb6bad1dc3c51",
+    "url": "/portfolioSite/static/css/main.f6cffc85.chunk.css"
+  },
+  {
+    "revision": "dccdd72990113260e605",
+    "url": "/portfolioSite/static/js/2.c9098fb4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolioSite/static/js/2.c9098fb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9aff79dcb6bad1dc3c51",
+    "url": "/portfolioSite/static/js/main.6d37cba3.chunk.js"
+  },
+  {
+    "revision": "4145ae13e20e30fec2a5",
+    "url": "/portfolioSite/static/js/runtime-main.36df7ddb.js"
+  }
+]);
